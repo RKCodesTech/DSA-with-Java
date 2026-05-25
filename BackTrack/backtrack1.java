7883154672c43f65b1b5ddd68d19a02b4596f678
RIYA KOSTA <riyakosta2609@gmail.com>
2026-05-25 23:10:02 +0530

    added comments

--- BackTrack/backtrack1.java
+++ BackTrack/backtrack1.java
@@ -2,13 +2,13 @@ package BackTrack;
 class backtack1{
 public static void PrintPermutation(String str, int index,String perm){
     if(str.length()==0){
-        System.out.println(perm);
+        System.out.println(perm);// base condition 
         return;
     }
     for(int i=0;i<str.length();i++){
-        char currentChar =str.charAt(i);
-        String newString=str.substring(0, i)+str.substring(i+1);
-        PrintPermutation(newString, index+1, currentChar+perm);
+        char currentChar =str.charAt(i);//pick current character help to remove it from str
+        String newString=str.substring(0, i)+str.substring(i+1); // remove current char
+        PrintPermutation(newString, index+1, currentChar+perm); //recursive call 
     }
 }
 public static void main(String[] args) {
