4c256e715f1c804fb3cc6b38ceaa0b5f16513f80
RIYA KOSTA <riyakosta2609@gmail.com>
2026-05-26 22:44:47 +0530

    created merged array

--- arrays/mergesortarr.java
+++ arrays/mergesortarr.java
@@ -5,12 +5,13 @@ public class mergesortarr {
     public static void main(String[] args) {
         int[] arr1={2,4,2,6,8,3,8};
         int[] arr2={9,5,8,3,9,3}; 
-        
-        Arrays.sort(arr1);
-        Arrays.sort(arr2);
-        for(int i=0;i<arr1.length;i++){
-            System.out.println(arr1[i]);
+        int[] mergedarr=new int[arr1.length+arr2.length];
+        System.arraycopy(arr1, 0, mergedarr, 0, arr1.length);
+        System.arraycopy(arr2, 0, mergedarr, arr1.length, arr2.length);
+        Arrays.sort(mergedarr);
+        for(int elements: mergedarr){
+            System.out.println(elements);
         }
-
+      
     }
 }
