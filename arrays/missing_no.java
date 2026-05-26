59b922479c9cf907de9ac507e95f49a6b5412b0e
RIYA KOSTA <riyakosta2609@gmail.com>
2026-05-26 23:11:34 +0530

    added new code for missing no.

--- arrays/missing_no.java
+++ arrays/missing_no.java
@@ -2,13 +2,22 @@
 // from 1 to n), where one number is missing, and prints the missing number. For example,
 // for the array [1, 2, 4, 5], the missing number is 3.
 package arrays;
+
 public class missing_no {
     public static void main(String[] args) {
-        int arr[]={1,2,3,5,6,8,9};
-        for(int i=0;i<arr.length;i++){
-            if(arr[i]!=i+1){
-                 System.out.println("Missing number: " + (i + 1));
+
+        int arr[] = {1,2,3,5,6,8,9};
+
+        int expected = 1;
+
+        for(int i = 0; i < arr.length; i++) {
+
+            if(arr[i] != expected) {
+                System.out.println("Missing number: " + expected);
+                i--; // stay on same element
             }
+
+            expected++;
         }
     }
 }
