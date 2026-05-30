41948892ffdc9cd5c0342bdb41cceeaeb08cc104
RKCodesTech <riyakosta2609@gmail.com>
2026-05-30 23:54:49 +0530

    updated code to show importance of generic

--- Generic/GenArrayList.java
+++ Generic/GenArrayList.java
@@ -10,6 +10,12 @@ public class GenArrayList {
         list.add("hello Riya");
         list.add(123);
 String s1 = (String) list.get(0);
-String s2 =(String) list.get(1);
+String s2 =(String) list.get(1); 
+ArrayList<String> list2 =new ArrayList<>();
+list2.add("hello ");
+list2.add("Riya");
+list2.get(0);
+list2.get(1);
+
     }
 }
