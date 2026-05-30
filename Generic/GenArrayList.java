79b1bc2636379e8e39c6317b5234de59284b5f5b
RKCodesTech <riyakosta2609@gmail.com>
2026-05-30 23:59:16 +0530

    added defination as comments in code

--- Generic/GenArrayList.java
+++ Generic/GenArrayList.java
@@ -5,13 +5,15 @@ import java.util.ArrayList;
 public class GenArrayList {
     public static void main(String[] args) {
 
-        ArrayList list = new ArrayList<>();
-
+        ArrayList list = new ArrayList<>();// in this code you'll get the execption because u are taking 
+        //any type of data in ArrayList that's not the problem, the problem is in line no.13
         list.add("hello Riya");
         list.add(123);
 String s1 = (String) list.get(0);
-String s2 =(String) list.get(1); 
-ArrayList<String> list2 =new ArrayList<>();
+String s2 =(String) list.get(1); // you are taking integer list element as string. by typecasting it will generate exception while run time
+
+
+ArrayList<String> list2 =new ArrayList<>(); // here we <String> define that all element will be string type.. so no exception and that is Generic
 list2.add("hello ");
 list2.add("Riya");
 list2.get(0);
