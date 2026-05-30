270d16ee671d571a2069e4f4d754490d9a5636fc
RKCodesTech <riyakosta2609@gmail.com>
2026-05-30 23:52:25 +0530

    added generic code

---
+++ Generic/GenArrayList.java
@@ -0,0 +1,15 @@
+package Generic;
+
+import java.util.ArrayList;
+
+public class GenArrayList {
+    public static void main(String[] args) {
+
+        ArrayList list = new ArrayList<>();
+
+        list.add("hello Riya");
+        list.add(123);
+String s1 = (String) list.get(0);
+String s2 =(String) list.get(1);
+    }
+}
