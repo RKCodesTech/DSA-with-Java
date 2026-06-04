decbc89a2c854ca7e98a710973ec8f1e2d3da6f7
RKCodesTech <riyakosta2609@gmail.com>
2026-06-04 21:21:37 +0530

    added rotation code

---
+++ arrays/RotateArray.java
@@ -0,0 +1,32 @@
+package arrays;
+
+public import java.util.*;
+
+public class RotateArray {
+    static void reverse(int[] arr, int l, int r) {
+        while (l < r) {
+            int temp = arr[l];
+            arr[l] = arr[r];
+            arr[r] = temp;
+            l++;
+            r--;
+        }
+    }
+
+    public static void rotate(int[] arr, int k) {
+        int n = arr.length;
+        k = k % n;
+
+        reverse(arr, 0, n - 1);
+        reverse(arr, 0, k - 1);
+        reverse(arr, k, n - 1);
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {1,2,3,4,5};
+        rotate(arr, 2);
+        System.out.println(Arrays.toString(arr));
+    }
+} {
+    
+}
