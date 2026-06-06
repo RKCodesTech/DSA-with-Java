7a0a96fb4f0062f5a18de588f02c785499cd3100
RKCodesTech <riyakosta2609@gmail.com>
2026-06-06 23:40:11 +0530

    added changes

--- arrays/Main.java
+++ arrays/Main.java
@@ -7,7 +7,7 @@ public class Main {
         for (int i = arr.length - 1; i > 0; i--) {
             arr[i] = arr[i - 1];
         }
-
+//this is an array
         arr[0] = last;
 
         for (int num : arr) {
