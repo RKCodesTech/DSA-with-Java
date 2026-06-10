3e0d5b6224d9e8efde8704f96c7d516d56c7b551
RKCodesTech <riyakosta2609@gmail.com>
2026-06-10 18:15:41 +0530

    max array

--- arrays/maximum.java
+++ arrays/maximum.java
@@ -3,16 +3,16 @@ package arrays;
 //maximum no. in array
 public class maximum {
     public static void main(String[] args) {
-        int num=0;
+        int max=0;
         int arr[]={1,2,3,6,7};
         for(int i=0; i<arr.length;i++){
-            if(num<arr[i]){
-                int temp=num;
-                num=arr[i];
+            if(max<arr[i]){
+                int temp=max;
+                max=arr[i];
                 arr[i]=temp;
             }
 
         }
-        System.out.println(num);
+        System.out.println(max);
     }
 }
