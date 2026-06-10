027946e8f4e962105f7e1d70f2797e11c480b123
RKCodesTech <riyakosta2609@gmail.com>
2026-06-10 18:12:04 +0530

    understand arr rotation

--- arrays/rotate.java
+++ arrays/rotate.java
@@ -1,11 +1,11 @@
-public class Main {
+public class rotate {
     public static void main(String[] args) {
-        int arr[] = {1, 2, 3, 4, 5};
+        int arr[] = {1, 2, 3, 4, 5,7,8,9};
 
         int last = arr[arr.length - 1];
 
         for (int i = arr.length - 1; i > 0; i--) {
-            arr[i] = arr[i - 1];//add another
+            arr[i] = arr[i - 1];
         }
 
         arr[0] = last;
