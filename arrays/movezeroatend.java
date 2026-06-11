f9543cf77e9162706798cfd3ba9180106612707d
RKCodesTech <riyakosta2609@gmail.com>
2026-06-11 23:00:44 +0530

    understand the logic behind move all 0's at the end

--- arrays/movezeroatend.java
+++ arrays/movezeroatend.java
@@ -8,7 +8,8 @@ public class movezeroatend {
         int[] arr={0,3,6,0,6,0,6,9,6,9,0,0};
         int index=0;
         for(int i=0;i<arr.length;i++){
-            if(arr[i]!=0){
+            if(arr[i]!=0){ 
+                
                 arr[index]=arr[i];
                 index++;
             }
