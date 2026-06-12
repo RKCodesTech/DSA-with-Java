2c0c99cc3157b59f52894fdf5251c65b4876b776
RKCodesTech <riyakosta2609@gmail.com>
2026-06-13 00:50:32 +0530

    highest no. in hash

--- hashmap/hashHighest.java
+++ hashmap/hashHighest.java
@@ -4,40 +4,42 @@ package hashmap;
 
 public class hashHighest {
     public static void main(String[] args) {
+            int largestnum=0;
+    int largestvalue=0;
 
         int arr[] = {2, 4, 2, 5, 3, 5, 2, 4, 5};
 
         HashMap<Integer, Integer> hash = new HashMap<>();
+for(int i=0;i<arr.length;i++){
+    int num=arr[i];
+    if(hash.containsKey(num)){
+        hash.put(num, hash.get(num)+1);
+    }else{
+        hash.put(num, 1);
 
-        // Step 1: Count frequencies
-        for (int i = 0; i < arr.length; i++) {
-
-            int num = arr[i];
-
-            if (hash.containsKey(num)) {
-                hash.put(num, hash.get(num) + 1);
-            } else {
-                hash.put(num, 1);
-            }
-        }
-
-        // Step 2: Find maximum frequency element
-        int maxFreq = 0;
-        int maxElement = 0;
-
-        for (int i = 0; i < arr.length; i++) {
-
-            int num = arr[i];
-
-            if (hash.get(num) > maxFreq) {
-                maxFreq = hash.get(num);
-                maxElement = num;
-            }
-        }
-
-        System.out.println("Element with highest frequency = " + maxElement);
-        System.out.println("Frequency = " + maxFreq);
     }
-} {
+}
+
+for(int i=0;i<arr.length;i++){
+
+    int num=arr[i];
+    if(hash.get(num)>largestvalue){
+        largestvalue=hash.get(num);
+        largestnum=num;
+    }
+}
+    System.out.println("Element with highest frequency = " + largestnum);
+System.out.println("Frequency = " + largestvalue);    
+
+    }}
+
+
+
+
+
+
+
+
+        
     
 }
--- hashmap/hashmap1.java
+++ hashmap/hashmap1.java
@@ -5,22 +5,18 @@ import java.util.HashMap;
 public class hashmap1 {
 
     public static void main(String[] args) {
-
-        int arr[] = {2, 4, 2, 5, 3, 5, 2, 4, 5};
-
-        HashMap<Integer, Integer> hash = new HashMap<>();
-
-        for (int i = 0; i < arr.length; i++) {
-
-            int num = arr[i];
-
-            if (hash.containsKey(num)) {
-                hash.put(num, hash.get(num) + 1);
-            } else {
-                hash.put(num, 1);
-            }
-        }
-
-        System.out.println(hash);
+int [] arr={1,1,3,2,3,2,2,4,3,2,3,3,3,3,2,2,3,2,2,};
+HashMap <Integer,Integer> hash=new HashMap<>();
+   for(int i=0;i<arr.length;i++){
+    int num=arr[i];
+    if (hash.containsKey(num)){
+        hash.put(num,hash.get(num)+1 );
+    }else{
+        hash.put(num,1);
+    }
+   }   
+   System.out.println(hash.get(1));
+   System.out.println(hash.get(2));
+   System.out.println(hash.get(3));
     }
 }//hash
