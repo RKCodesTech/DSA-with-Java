21f5be62753ae12dd66be70aa391b455ec812955
RKCodesTech <riyakosta2609@gmail.com>
2026-06-12 23:43:37 +0530

    hashmap array

--- hashmap/hashmap1.java
+++ hashmap/hashmap1.java
@@ -1,5 +1,26 @@
 package hashmap;
 
+import java.util.HashMap;
+
 public class hashmap1 {
-    
+
+    public static void main(String[] args) {
+
+        int arr[] = {2, 4, 2, 5, 3, 5, 2, 4, 5};
+
+        HashMap<Integer, Integer> hash = new HashMap<>();
+
+        for (int i = 0; i < arr.length; i++) {
+
+            int num = arr[i];
+
+            if (hash.containsKey(num)) {
+                hash.put(num, hash.get(num) + 1);
+            } else {
+                hash.put(num, 1);
+            }
+        }
+
+        System.out.println(hash);
+    }
 }
