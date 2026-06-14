8a834f5ac998ee2cf6c5800978d032a8cb41c202
RKCodesTech <riyakosta2609@gmail.com>
2026-06-14 23:04:07 +0530

    bubble sort with timecomplexity and space complexity

--- sorting/bubblesort.java
+++ sorting/bubblesort.java
@@ -1,9 +1,9 @@
 package sorting;
 public class bubblesort {
 public static void main(String[] args) {
-    int arr[]={2,5,3,7,58,9};
-    for(int k=0; k<=arr.length-1;k++){
-    for(int i=0;i<arr.length-1;i++){
+    int arr[]={2,5,3,7,5,8,9};
+    for(int k=0; k<arr.length;k++){
+    for(int i=0;i<arr.length;i++){
         if(arr[i]>arr[i+1]){
             int temp =arr[i];
             arr[i]=arr[i+1];
@@ -16,3 +16,11 @@ public static void main(String[] args) {
     }
 }
 }
+
+// TC-outer loop=n times
+//    inner loop =n-1 times 
+//     nx(n-1)=n^2
+//     o(n^2)
+
+// SC - length is not depend on the input , and no extra variable
+//      o(1)
