40d124d9199ef89043435e6821716df6e8656423
RKCodesTech <riyakosta2609@gmail.com>
2026-06-15 22:34:06 +0530

    javaMath

--- JavaMath/MathDemo.java
+++ JavaMath/MathDemo.java
@@ -1,4 +1,4 @@
-package javaMath;
+
 
 public class MathDemo {
     public static void main(String[] args) {
@@ -6,6 +6,14 @@ public class MathDemo {
         // Math.max()
         System.out.println("Maximum: " + Math.max(5, 10));
 
+          // Math.min()
+        System.out.println("Minimum: " + Math.min(5, 10));
+
+        // Math.sqrt()
+        System.out.println("Square Root of 64: " + Math.sqrt(64));
+
+        // Math.abs()
+        System.out.println("Absolute Value of -4.7: " + Math.abs(-4.7));
         
     }
 }
