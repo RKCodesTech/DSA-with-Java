4704dcde8f49b0f7fa9c38b4c3412529251c6594
RKCodesTech <riyakosta2609@gmail.com>
2026-06-15 22:33:07 +0530

    javaMath

---
+++ JavaMath/MathDemo.java
@@ -0,0 +1,11 @@
+package javaMath;
+
+public class MathDemo {
+    public static void main(String[] args) {
+
+        // Math.max()
+        System.out.println("Maximum: " + Math.max(5, 10));
+
+        
+    }
+}
