9a4be70f582e7328c6ee4802accf360b0d9a4ddf
RKCodesTech <riyakosta2609@gmail.com>
2026-06-16 23:52:26 +0530

    java math

---
+++ JavaMath/MathDemo2.java
@@ -0,0 +1,13 @@
+public class MathDemo2 {
+    public static void main(String[] args) {
+
+        // Math.cbrt() - Cube Root
+        System.out.println("Cube Root of 27: " + Math.cbrt(27));
+
+        // Math.signum() - Sign of a number
+        System.out.println("Sign of -15: " + Math.signum(-15));
+        System.out.println("Sign of 20: " + Math.signum(20));
+
+   
+    }
+}
