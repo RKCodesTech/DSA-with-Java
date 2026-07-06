bbe855d43b191c3d5229df90414fee922f897266
RKCodesTech <riyakosta2609@gmail.com>
2026-07-07 01:16:15 +0530

    Creted Linked List

---
+++ LinkedList/createlinkedlist.java
@@ -0,0 +1,22 @@
+package LinkedList;
+class Node{
+    int data;
+    Node  Next;
+    public Node(int data){
+        this.data=data;
+    }
+}
+public class createlinkedlist {
+   public static void main(String[] args) {
+    Node head= new Node(1);
+    Node n1=new Node(2);
+    Node n2= new Node(3);
+     head.Next=n1;
+     n1.Next=n2;
+     Node temp = head;
+     while(temp!=null){
+        System.out.println(temp.data);
+        temp=temp.Next;
+     }
+   } 
+}
