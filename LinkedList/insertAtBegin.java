5604f0babafa20a651e1356bfc1ace1c8fb7129d
RKCodesTech <riyakosta2609@gmail.com>
2026-07-09 00:38:33 +0530

    Linked list insert At the Begin

--- LinkedList/insertAtBegin.java
+++ LinkedList/insertAtBegin.java
@@ -1,5 +1,7 @@
 package LinkedList;
 
+import java.nio.channels.Pipe.SourceChannel;
+
 class Node {
     int data;
     Node next;
@@ -14,7 +16,17 @@ class Node {
         Node n1 =new Node(34);
         Node n2=new Node(4);
         Node n3=new Node(234);
-        head.next=n2
+        head.next=n2;
+        n1.next=n2;
+        n2.next=n3;
+        //inserting new Node;
+        Node newNode=new Node(1);
+        newNode.next=head;
+        head=newNode;
+        int temp =head;
+        while(head==null){
+            System.out.println();
+        }
     }
 } 
     
