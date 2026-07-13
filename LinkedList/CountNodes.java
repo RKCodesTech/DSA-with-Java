03a79a818cd49565b4cc7b7c5316aa06f75c40f8
RKCodesTech <riyakosta2609@gmail.com>
2026-07-13 23:50:27 +0530

    Sucessfully Counted the Nodes in linkedList

--- LinkedList/CountNodes.java
+++ LinkedList/CountNodes.java
@@ -1,1 +1,41 @@
-add
+public class CountNodes {
+
+    static class Node {
+        int data;
+        Node next;
+
+        Node(int data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    Node head;
+
+    // Count total nodes
+    public void countNodes() {
+
+        Node temp = head;
+        int count = 0;
+
+        while (temp != null) {
+            count++;
+            temp = temp.next;
+        }
+
+        System.out.println("Total Nodes = " + count);
+    }
+
+    public static void main(String[] args) {
+
+        CountNodes list = new CountNodes();
+
+        // Creating Linked List
+        list.head = new Node(10);
+        list.head.next = new Node(20);
+        list.head.next.next = new Node(30);
+        list.head.next.next.next = new Node(40);
+
+        list.countNodes();
+    }
+}
