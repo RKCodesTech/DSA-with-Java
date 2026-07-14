0739746cc7117f7a5fe990656be9b7482ddabff8
RKCodesTech <riyakosta2609@gmail.com>
2026-07-14 23:54:41 +0530

    created file Print linkedlist.java

---
+++ LinkedList/PrintLinkedList.java
@@ -0,0 +1,36 @@
+public class PrintLinkedList {
+
+    static class Node {
+        int data;
+        Node next;
+
+        Node(int data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    Node head;
+
+    // Print Linked List
+    public void printList() {
+        Node temp = head;
+
+        while (temp != null) {
+            System.out.print(temp.data + " ");
+            temp = temp.next;
+        }
+    }
+
+    public static void main(String[] args) {
+
+        PrintLinkedList list = new PrintLinkedList();
+
+        list.head = new Node(10);
+        list.head.next = new Node(20);
+        list.head.next.next = new Node(30);
+        list.head.next.next.next = new Node(40);
+
+        list.printList();
+    }
+}
---
+++ arrays/secondLar.java
@@ -0,0 +1,8 @@
+package arrays;
+
+public class secondLar {
+  public static void main (String[] args){
+    int[] arr={3,6,2,6};
+    int largest= 
+  }  
+}
