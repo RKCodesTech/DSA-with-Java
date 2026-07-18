0ed57e09460bdaeaf7542427b540959c493504aa
RKCodesTech <riyakosta2609@gmail.com>
2026-07-18 23:48:18 +0530

    Implemented LinkedListTraversal.java

---
+++ LinkedList/LinkedListTraversal.java
@@ -0,0 +1,35 @@
+public class LinkedListTraversal {
+
+    static class Node {
+        int data;
+        Node next;
+
+        Node(int data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    public static void main(String[] args) {
+
+        // Create nodes
+        Node first = new Node(10);
+        Node second = new Node(20);
+        Node third = new Node(30);
+
+        // Connect nodes
+        first.next = second;
+        second.next = third;
+
+        // Head points to first node
+        Node head = first;
+
+        // Traverse and print
+        Node temp = head;
+
+        while (temp != null) {
+            System.out.println(temp.data);
+            temp = temp.next;
+        }
+    }
+}
