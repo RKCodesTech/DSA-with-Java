f4b4eee420b9a9cae6db78893627b44dfbbf1636
RKCodesTech <riyakosta2609@gmail.com>
2026-07-20 23:12:46 +0530

    simplify, DeleteLastNode.java

--- LinkedList/DeleteLastNode.java
+++ LinkedList/DeleteLastNode.java
@@ -1,55 +1,24 @@
-public class DeleteLastNode {
+class DeleteLastNode {
 
-    // Node class
     static class Node {
         int data;
         Node next;
 
         Node(int data) {
             this.data = data;
-            this.next = null;
         }
     }
 
     Node head;
 
-    // Delete last node
-    public void deleteLast() {
-
-        // Case 1: Empty list
-        if (head == null) {
-            System.out.println("Linked List is empty.");
-            return;
-        }
-
-        // Case 2: Only one node
-        if (head.next == null) {
-            head = null;
-            return;
-        }
-
-        // Case 3: More than one node
+    void deleteLast() {
         Node temp = head;
 
-        // Move to second last node
         while (temp.next.next != null) {
             temp = temp.next;
         }
 
-        // Delete last node
         temp.next = null;
-    }
-
-    // Display Linked List
-    public void display() {
-        Node temp = head;
-
-        while (temp != null) {
-            System.out.print(temp.data + " -> ");
-            temp = temp.next;
-        }
-
-        System.out.println("NULL");
     }
 
     public static void main(String[] args) {
@@ -61,12 +30,6 @@ public class DeleteLastNode {
         list.head.next.next = new Node(30);
         list.head.next.next.next = new Node(40);
 
-        System.out.println("Before Deletion:");
-        list.display();
-
         list.deleteLast();
-
-        System.out.println("After Deletion:");
-        list.display();
     }
 }
