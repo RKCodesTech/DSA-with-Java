3d4e93684535eef85eed8b271970e3a2fdfdee37
RKCodesTech <riyakosta2609@gmail.com>
2026-07-20 23:01:00 +0530

    Simplify code DeleteFirstNode

--- LinkedList/DeleteFirstNode.java
+++ LinkedList/DeleteFirstNode.java
@@ -1,4 +1,4 @@
-public class DeleteFirstNode {
+class DeleteFirstNode {
 
     static class Node {
         int data;
@@ -6,48 +6,27 @@ public class DeleteFirstNode {
 
         Node(int data) {
             this.data = data;
-            this.next = null;
         }
     }
 
     Node head;
 
-    public void deleteFirst() {
+    void deleteFirst() {
         if (head == null) {
-            System.out.println("Linked List is Empty");
-            return;
+            System.out.println("List is Empty");
+        } else {
+            head = head.next;
         }
-
-        head = head.next;
-    }
-
-    public void display() {
-        Node temp = head;
-
-        while (temp != null) {
-            System.out.print(temp.data + " -> ");
-            temp = temp.next;
-        }
-
-        System.out.println("NULL");
     }
 
     public static void main(String[] args) {
 
         DeleteFirstNode list = new DeleteFirstNode();
 
-        // Creating the linked list manually
         list.head = new Node(10);
         list.head.next = new Node(20);
         list.head.next.next = new Node(30);
-        list.head.next.next.next = new Node(40);
-
-        System.out.println("Before Deletion:");
-        list.display();
 
         list.deleteFirst();
-
-        System.out.println("After Deletion:");
-        list.display();
     }
 }
