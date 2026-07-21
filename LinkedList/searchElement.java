8ee3c2ebccde84231d6bd0bc3a1d77cd1b5fde97
RKCodesTech <riyakosta2609@gmail.com>
2026-07-21 23:40:14 +0530

    simplify searchElement.java

--- LinkedList/searchElement.java
+++ LinkedList/searchElement.java
@@ -1,19 +1,17 @@
-public class searchElement {
+public class SearchElement {
 
-    static class Node {
+    class Node {
         int data;
         Node next;
 
         Node(int data) {
             this.data = data;
-            this.next = null;
         }
     }
 
     Node head;
 
-    // Insert at End
-    public void insert(int data) {
+    void insert(int data) {
 
         Node newNode = new Node(data);
 
@@ -31,51 +29,31 @@ public class searchElement {
         temp.next = newNode;
     }
 
-    // Search Element
-    public void search(int key) {
+    void search(int key) {
 
         Node temp = head;
-        int position = 1;
 
         while (temp != null) {
 
             if (temp.data == key) {
-                System.out.println("Element Found at Position : " + position);
+                System.out.println("Element Found");
                 return;
             }
 
             temp = temp.next;
-            position++;
         }
 
         System.out.println("Element Not Found");
     }
 
-    // Display
-    public void display() {
-
-        Node temp = head;
-
-        while (temp != null) {
-            System.out.print(temp.data + " -> ");
-            temp = temp.next;
-        }
-
-        System.out.println("null");
-    }
-
     public static void main(String[] args) {
 
-        searchElement list = new searchElement();
+        SearchElement list = new SearchElement();
 
         list.insert(10);
         list.insert(20);
         list.insert(30);
-        list.insert(40);
 
-        list.display();
-
-        list.search(30);
-        list.search(100);
+        list.search(20);
     }
 }
