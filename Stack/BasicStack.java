00d383a96c3a5e3cc4d39b4f6463feb9ae8b2270
RKCodesTech <riyakosta2609@gmail.com>
2026-09-06 22:29:44 +0530

    basic code of stack

---
+++ Stack/BasicStack.java
@@ -0,0 +1,15 @@
+package Stack;
+import java.util.Stack;
+public class BasicStack {
+    public static void main(String[] args) {   
+    Stack<String> st=new Stack<>();
+    st.push("riya");
+    st.push("yashvi");
+    st.push("pari");
+    st.push("aarav");
+    st.push("ansh");
+    st.push("kittu");
+    System.out.println(st.size());
+        System.out.println(st);
+}
+    }
