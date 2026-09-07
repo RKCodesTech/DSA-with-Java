a43516a51b74b45b99a5d705a68a47fe4af2b898
RKCodesTech <riyakosta2609@gmail.com>
2026-09-07 23:14:52 +0530

    print element by index

---
+++ Stack/ElementAtPosition.java
@@ -0,0 +1,21 @@
+package Stack;
+import java.util.Stack;
+public class ElementAtPosition {
+    public static void main(String[] args) {
+        
+    Stack<Integer> st= new Stack<>();
+    Stack<Integer> st2= new Stack<>();
+    int index=3;
+    st.push(6);//0
+     st.push(1);//1
+     st.push(2);//2
+     st.push(4);//3
+     while(index<st.size()){
+       st2.push(st.pop());
+     }
+    System.out.println(st.peek());
+    
+    
+    
+    }
+}
