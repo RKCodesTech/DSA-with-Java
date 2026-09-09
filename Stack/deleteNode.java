636e5e682e85218b6ce0ee525788c24da5be6b0c
RKCodesTech <riyakosta2609@gmail.com>
2026-09-10 00:04:04 +0530

    Delete any element

--- Stack/deleteNode.java
+++ Stack/deleteNode.java
@@ -9,14 +9,16 @@ public static void main(String[] args) {
     st.push(4);//store integer values in st
     st.push(6);          
     st.push(2);
-    while(st.size()>index){ //i
-        st2.push(st.pop());
+    while(st.size()>index){ //dlt element until it reaches the index
+        st2.push(st.pop());//now dlt from st and push into st
 
     }
-    st.pop();
-    while(st2.size()>0){
-        st.push(st2.pop());
+    st.pop();//dlt top element
+    while(st2.size()>0){ // run loop until st2 become empty
+        st.push(st2.pop());//dlt from st2 and push into st
     }
     System.out.println(st);
 }   
 }
+//TC- o(n)
+//SC- o(n)
