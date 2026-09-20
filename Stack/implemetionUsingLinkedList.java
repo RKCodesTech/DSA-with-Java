a76cb3e07c5ee7301fae954e136b5cf156dfebe7
RKCodesTech <riyakosta2609@gmail.com>
2026-09-20 23:58:35 +0530

    created file of implementaion of stack using linked List

--- Stack/implemetionUsingLinkedList.java
+++ Stack/implemetionUsingLinkedList.java
@@ -1,6 +1,28 @@
 package Stack;
 
+class Node{
+    int val;
+    Node next;
+    Node(int val){
+        this.val=val;
+    }
+}
+class MyStack{
+    Node head;
+    int len;
+    int peek(){
+        if()
+    return head.val;
+    }
+    int pop(){//delete at head
+        int x=head.val;
+        head=head.next;
+        return x;
+    }
+    int p
+}
 public class implemetionUsingLinkedList {
+   
     public static void main(String[] args) {
     
     }
